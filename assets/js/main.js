e31a773f7ff187131e496052c2f91316a4f6f38b
guilhermedevmatos15 <devguilhermeferreira@gmail.com>
2023-03-23 17:33:47 -0300

    Resolvendo e avisando problema na library typed

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,5 @@ import initSmoothScroll from './library/smoothScroll.js';
 import initTypedTexts from './library/typed.js';
 initAOS(1800); // 1000 === 1 segundo
 initSmoothScroll(0, 2000);
-initTypedTexts(['one', 'two', 'tree'], 300, 300, 400);
+// ! Se não utilizar a library typed, deixe comentada para não gerar erros. Se quiser usar apenas descomente a linha abaixo e configure da maneira que quiser
+// initTypedTexts(['one', 'two', 'tree'], 300, 300, 400);
